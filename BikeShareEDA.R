298e060db6ee2d20c60b973abaa6f9831d98736f
unknown <michaelgball7@gmail.com>
2023-09-16 11:03:53 -0600

    updated EDA

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -2,6 +2,22 @@
 
 library(tidyverse)
 library(vroom)
+library(ggplot2)
+library(gridExtra)
 
-
+#Read data
 bike <- read.csv("./Stat348/Kaggle-Bike-Share/train.csv")
+
+#Overview
+dplyr::glimpse(bike)
+skimr::skim(bike)
+
+#plots
+bike$season <- as.character(bike$season)
+bike$weather <- as.character(bike$weather)
+
+seasonplot <- ggplot(data=bike, aes(x=season, y=count))+geom_boxplot()
+weatherplot <- ggplot(bike, aes(weather, casual))+geom_boxplot()
+tempplot <- ggplot(bike,aes(temp, count))+geom_point()+geom_smooth(se=FALSE)
+atempplot <- ggplot(bike, aes(atemp, registered))+geom_point()+geom_smooth(se=FALSE)
+grid.arrange(seasonplot,weatherplot,tempplot,atempplot, nrow=2,ncol=2)
