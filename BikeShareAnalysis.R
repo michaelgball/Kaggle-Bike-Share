b3359389cf86fb8c3066dbc3b2c1abd82305f25e
unknown <michaelgball7@gmail.com>
2023-09-17 21:54:29 -0600

    Analysis

---
+++ BikeShareAnalysis.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(tidymodels)
+library(vroom)
+## Read in the data
+bikeTrain <- read.csv("./Stat348/Kaggle-Bike-Share/train.csv")
+bikeTest <- read.csv("./Stat348/Kaggle-Bike-Share/test.csv")
+## Remove casual and registered because we can't use them to predict
+bikeTrain <- bikeTrain %>%
+  select(-casual, - registered)
+bikeTrain$datetime <- as_datetime(bikeTrain$datetime)
+bikeTest$datetime <- as_datetime(bikeTest$datetime)
+## Cleaning & Feature Engineering
+bike_recipe <- recipe(count~., data=bikeTrain) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(datetime)
+prepped_recipe <- prep(bike_recipe)
+bake(prepped_recipe, new_data = bikeTrain) #Make sure recipe work on train
+bake(prepped_recipe, new_data = bikeTest) #Make sure recipe works on test
+
+## Define the model
+lin_model <- linear_reg() %>%
+  set_engine("lm")
+## Set up the whole workflow
+bike_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(lin_model) %>%
+  fit(data=bikeTrain)
+## Look at the fitted LM model this way
+extract_fit_engine(bike_workflow) %>%
+  summary()
+## Get Predictions for test set AND format for Kaggle
+test_preds <- predict(bike_workflow, new_data = bikeTest) %>%
+  bind_cols(., bikeTest) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and predictions
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+## Write prediction file to CSV
+vroom_write(x=test_preds, file="./TestPreds.csv", delim=",") 
--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -1,23 +1,25 @@
-##Bike Share EDA Code
+##bikeTrain Share EDA Code
 
 library(tidyverse)
 library(vroom)
 library(ggplot2)
 library(gridExtra)
+library(tidymodels)
 
 #Read data
-bike <- read.csv("./Stat348/Kaggle-Bike-Share/train.csv")
+bikeTrain <- read.csv("./Stat348/Kaggle-Bike-Share/train.csv")
+bikeTest <- read.csv("./Stat348/Kaggle-Bike-Share/test.csv")
 
 #Overview
-dplyr::glimpse(bike)
-skimr::skim(bike)
+dplyr::glimpse(bikeTrain)
+skimr::skim(bikeTrain)
 
 #plots
-bike$season <- as.character(bike$season)
-bike$weather <- as.character(bike$weather)
+bikeTrain$season <- as.character(bikeTrain$season)
+bikeTrain$weather <- as.character(bikeTrain$weather)
 
-seasonplot <- ggplot(data=bike, aes(x=season, y=count))+geom_boxplot()
-weatherplot <- ggplot(bike, aes(weather, casual))+geom_boxplot()
-tempplot <- ggplot(bike,aes(temp, count))+geom_point()+geom_smooth(se=FALSE)
-atempplot <- ggplot(bike, aes(atemp, registered))+geom_point()+geom_smooth(se=FALSE)
+seasonplot <- ggplot(data=bikeTrain, aes(x=season, y=count))+geom_boxplot()
+weatherplot <- ggplot(bikeTrain, aes(weather, casual))+geom_boxplot()
+tempplot <- ggplot(bikeTrain,aes(temp, count))+geom_point()+geom_smooth(se=FALSE)
+atempplot <- ggplot(bikeTrain, aes(atemp, registered))+geom_point()+geom_smooth(se=FALSE)
 grid.arrange(seasonplot,weatherplot,tempplot,atempplot, nrow=2,ncol=2)
